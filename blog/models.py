e58abc3b7ab4103828437e2ed6ee3c9b7e367a88
Prima Kurniawan <primakurniawan666@gmail.com>
2022-11-10 06:38:37 +0700

    Adding comment feature

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,8 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -19,3 +20,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.TimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.content
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
+from .models import Post, Comment
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
-    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_list(request):
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post = get_object_or_404(Post, pk=pk)
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.created_at = timezone.now()
+            comment.post = post
+            comment.user = request.user
 
-    return render(request, 'blog/post_detail.html', {'post':post})
+            comment.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        post = get_object_or_404(Post, pk=pk)
+        form = CommentForm(instance=post)
+        comments = Comment.objects.all().filter(post__pk=pk)
+
+        return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -26,6 +46,7 @@ def post_new(request):
     else:
         form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
